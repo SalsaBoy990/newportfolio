088119168082e88370b31d490bd7138d5ed67e46
Gulácsi András <guland@protonmail.com>
2023-11-04 02:39:33 +0100

    add: Clean sass files and the js script for the module

---
+++ modules/frontpage/_dev/js/frontpage.js
@@ -0,0 +1,24 @@
+window.ready = function (handler) {
+    if (/complete|loaded|interactive/.test(document.readyState) && document.body) {
+        handler();
+    } else {
+        document.addEventListener("DOMContentLoaded", handler, false);
+    }
+};
+
+window.ready(function () {
+
+    console.log('script works...')
+
+    /* Scroll to top button */
+    var scrollToTopBtn = document.getElementById("scroll-to-top");
+    scrollToTopBtn.addEventListener("click", function () {
+        console.log(this);
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        });
+    });
+
+    // Other functionality...
+});
