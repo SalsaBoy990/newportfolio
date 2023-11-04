7ab51369c9b12d29080e37877c0709d960231ac0
Gulácsi András <guland@protonmail.com>
2023-11-04 01:50:01 +0100

    new: Localization module for multi-language websites

---
+++ modules/localization/controllers/Localization.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Language Switcher
+ */
+class Localization extends Trongate
+{
+    /**
+     * Change website language
+     * stores its value in a cookie
+     *
+     * @param  string  $lang
+     *
+     * @return void
+     */
+    public function language(string $lang): void
+    {
+        settype($lang, 'string');
+        setcookie('language', htmlspecialchars($lang), time() + (86400 * 30), "/");
+        redirect('frontpage/index');
+    }
+
+
+    public function test(): void
+    {
+        $params      = file_get_contents('php://input');
+        $posted_data = (object) json_decode($params);
+
+        var_dump($posted_data);
+    }
+}
