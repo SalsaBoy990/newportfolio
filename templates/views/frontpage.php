a4650e74cc3c796dfe4db9e13a7beb03eaf627f5
Gulácsi András <guland@protonmail.com>
2023-11-06 22:47:57 +0100

    add favicons for meta tags

--- templates/views/frontpage.php
+++ templates/views/frontpage.php
@@ -24,11 +24,11 @@
     <meta name="twitter:url" content=""/>
     <meta name="twitter:image" content=""/>
 
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-    <link rel="manifest" href="/site.webmanifest">
-    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5b71d5">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?= BASE_URL?>/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?= BASE_URL?>/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?= BASE_URL?>/favicon-16x16.png">
+    <link rel="manifest" href="<?= BASE_URL?>/site.webmanifest">
+    <link rel="mask-icon" href="<?= BASE_URL?>/safari-pinned-tab.svg" color="#5b71d5">
     <meta name="apple-mobile-web-app-title" content="Andr&aacute;s Gul&aacute;csi's Portfolio">
     <meta name="application-name" content="Andr&aacute;s Gul&aacute;csi's Portfolio">
     <meta name="msapplication-TileColor" content="#ffffff">
