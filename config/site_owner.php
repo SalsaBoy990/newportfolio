7c3b6f88193d923611970f3e8e0e4ed9daf236dc
Gulácsi András <guland@protonmail.com>
2024-02-21 03:10:00 +0100

    Add CV pdf file to download

--- config/site_owner.php
+++ config/site_owner.php
@@ -1,7 +1,8 @@
 <?php
 //Settings to do with the identification of the site owner
-define('WEBSITE_NAME', '');
+define('WEBSITE_NAME', 'Portfólió');
 define('OUR_NAME', 'Your Company Name');
 define('OUR_TELNUM', '+36204427225');
 define('OUR_ADDRESS', '');
 define('OUR_EMAIL_ADDRESS', 'gandras@passmail.net');
+define('MY_CV_LINK', 'frontpage_module/pdf/andras-gulacsi-php-dev-2024-01-27-cv.pdf');
