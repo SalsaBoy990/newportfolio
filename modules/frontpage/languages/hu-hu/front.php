c311c56202ee81a596345396a815f62857fe5c2f
Gulácsi András <guland@protonmail.com>
2024-02-21 03:00:10 +0100

    Update Clean.UI CSS, update texts

--- modules/frontpage/languages/hu-hu/front.php
+++ modules/frontpage/languages/hu-hu/front.php
@@ -3,16 +3,16 @@
 /* general */
 $t['text_name']             = 'Gulácsi András';
 $t['text_title']            = 'Fullstack PHP fejlesztő vagyok,';
-$t['text_subtitle']         = 'landing oldalakat, vállalati weboldalakat, e-kereskedelmi üzleteket és egyedi appokat készítek.';
+$t['text_subtitle']         = 'landing oldalakat, vállalati weboldalakat, webshopokat és egyedi webappokat készítek.';
 $t['text_greeting']         = 'Üdvözöllek, Gulácsi András vagyok.';
-$t['text_introduction']     = 'I have four years of work experience of commercial web development. I worked at smaller, local companies, startups, and did some remote contract work too. I am familiar with frontend and PHP backend roles. I consider myself a full-stack developer based on my current skill set.';
+$t['text_introduction']     = '5 év webfejlesztői tapasztalattal rendelkezem, 2019 óta készítek másoknak weboldalakat. Távmunkában / irodában, alkalmazottként / vállalkozóként is dolgoztam mind frontend, mind backend szerepkörben.';
 $t['text_skills_heading']   = 'Kiemelt képességek';
 $t['text_projects_heading'] = 'Projektmunkáim';
 $t['text_copyright']        = '© Gulácsi András 2022-2023. Minden jog fenntartva!';
 $t['text_construction']     = 'Az oldal építés alatt!';
 $t['text_send']             = 'E-mail küldése';
 $t['text_download']         = 'CV letöltése';
-$t['text_goto_top']         = 'Go to the top of the page';
+$t['text_goto_top']         = 'Ugrás az oldal tetejére';
 
 
 /* meta information */
