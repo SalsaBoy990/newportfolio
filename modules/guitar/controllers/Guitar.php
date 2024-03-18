5cf7e7b08f1d17ff239bebff037e558b4a36d2f8
Gulácsi András <guland@protonmail.com>
2024-03-18 20:33:21 +0100

    Add a simple demo module called guitar

---
+++ modules/guitar/controllers/Guitar.php
@@ -0,0 +1,11 @@
+<?php
+
+class Guitar extends Trongate
+{
+    function information()
+    {
+        $data['guitar_title'] = 'Fender Telecaster';
+        $this->view('information', $data);
+    }
+
+}
