78e3f999606b9fb5fab08ede90507cfa3aff21c0
Gulácsi András <guland@protonmail.com>
2024-04-15 23:58:08 +0200

    Update CVs (add English and Hungarian versions as well)

--- config/site_owner.php
+++ config/site_owner.php
@@ -5,4 +5,5 @@ define('OUR_NAME', 'Your Company Name');
 define('OUR_TELNUM', '+36204427225');
 define('OUR_ADDRESS', '');
 define('OUR_EMAIL_ADDRESS', 'gandras@passmail.net');
-define('MY_CV_LINK', 'frontpage_module/pdf/andras-gulacsi-php-dev-cv-2024-03-18.pdf');
+define('MY_ENGLISH_CV_LINK', 'frontpage_module/pdf/AndrasGulacsiCV2024English.pdf');
+define('MY_HUNGARIAN_CV_LINK', 'frontpage_module/pdf/GulacsiAndrasCV2024Magyar.pdf');
